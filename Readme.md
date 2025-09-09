00c575ef938178f38cbd63869b8f5b160225579c
TamannaNasrin04 <tamannanasrin1604@gmail.com>
2025-09-09 11:40:33 +0600

    added readme.md file

---
+++ Readme.md
@@ -0,0 +1,23 @@
+difference between getElementById, getElementsByClassName, and querySelector / querySelectorAll :
+                * getElementById is used to select a single element by its id.It also returns a single element .
+
+                * getElementsByClassName is used to selects all elements with a given class name And returns HTMLCollection.
+               
+                * querySelector is used to select the first element that matches a CSS selector and returns a single element
+                 
+                * querySelectorAll is used to selects all elements matching a CSS selector and returns a NodeList.
+
+
+document.createElement() is used to create a new HTML element. ANd appendChild() is used to insert into an existing element that is already part of the DOM.
+
+
+Event bubbling works when an event happens on an element and then moves up to its parent,to parent's parent, and so on.
+
+
+Event delegation works when one event listener is added to a parent element instead of many listeners to each child.
+
+
+Difference between preventDefault() and stopPropagation() :
+                    * preventDefault() stops the browser’s default action.
+
+                    * stopPropagation() stops the event from bubbling up to parent elements.
--- script.js
+++ script.js
@@ -14,4 +14,90 @@ window.onload = function(e){
 
 
 
+// document.addEventListener('DOMContentLoaded', function(){
+//     const coin = document.getElementById("coin");
+//     const callHistory = document.getElementById("clearHistory");
+//     const historyClear = document.getElementById("clearHistoryBtn");
+//     const callButtons = document.querySelectorAll(".bg-[#00A63E].text-white");
 
+//     for(button of callButtons){
+//         button.addEventListener('click', function(){
+//             const card = button.closest(".space-y-3");
+//             const name = card.querySelector("h3.font-bold.text-xl").innerText;
+//             const number = card.querySelector("h3.font-bold.text-2xl").innerText;
+
+//         let currentCoins = parseInt(coin.innerText);
+
+//         if(currentCoins < 20){
+//             alert("You don't have enough coins to make a call.");
+//             return;
+//         }
+//         currentCoins -= 20;
+//         coin.innerText = currentCoins;
+//         alert(`Calling ${name}  ${number}`);
+
+//     })
+// }
+
+// })
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    const coin = document.getElementById("coin");
+    const callHistory = document.getElementById("clearHistory");
+    const historyClear = document.getElementById("clearHistoryBtn");
+    const allButtons = document.getElementsByTagName('button');
+
+    for (let i = 0; i < allButtons.length; i++){
+        const button = allButtons[i];
+        const span = button.getElementsByTagName("span")[0];
+        if (span && span.innerText.trim().toLowerCase() === "call") {
+            button.onclick = function () {
+                handleCall(this);
+            };
+        }
+    }
+    function handleCall(buttonElement){
+        const card = buttonElement.closest(".space-y-3");
+        const name = card.querySelector("h3.font-bold.text-xl").innerText;
+        const number = card.querySelector("h3.font-bold.text-2xl").innerText;
+
+        let currentCoins = parseInt(coin.innerText);
+
+        if(currentCoins < 20){
+            alert("You don't have enough coins to make a call.");
+            return;
+        }
+        alert("📞 Calling " + name + " " + number);
+        
+        currentCoins -= 20;
+        coin.innerText = currentCoins;
+
+        const time = new Date().toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: true
+        });
+
+        addToHistory(name, number, time);
+    }
+    function addToHistory(name, number, time){
+        const historyItem = document.createElement("div");
+        historyItem.className = "bg-gray-100 p-3 rounded-md text-base ";
+        historyItem.innerHTML = `
+            <div class="flex justify-between items-center font-semibold text-[#111] mb-1">
+            <span>${name}</span>
+            <span class="text-gray-500 text-xs">${time}</span>
+            </div>
+            <div class="text-gray-700 text-sm">${number}</div>
+        `;
+        callHistory.prepend(historyItem);
+    }
+    historyClear.addEventListener("click", function(){
+    callHistory.innerHTML = "";
+    });
+});
